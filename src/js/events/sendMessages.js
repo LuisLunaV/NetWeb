6388ea7e715ac066de43e3c38d48137321a16021
LuisLunaV <luis.a.luna.v@outlook.com>
2025-10-11 21:19:16 -0600

    pdf

--- src/js/events/sendMessages.js
+++ src/js/events/sendMessages.js
@@ -1,22 +1,25 @@
-import { sendMessagePost } from '../services/apiPost.js';
-import { sanitizePayload } from '../helpers/sanitizacionDeObjetos.js';
-import {  messageSendAlert } from '../utils/sweetsAlerts.js';
-export const sendMessage=( formContact)=>{
-    formContact.addEventListener('submit', async( e )=>{
-        e.preventDefault();
-        const formData = new FormData(formContact);
-        const payload = Object.fromEntries(formData);
-        const data = sanitizePayload(payload);
-        if( !data) return alert('No debe haber datos vacios en el formulario.');
-        const resp = await sendMessagePost(data);
-        if(resp){
-            messageSendAlert();
-        }
-        
-        const cajas = formContact.querySelectorAll('input, textarea');
-        cajas.forEach(element => {
-            console.log(element)
-            element.value = '';
-        });
-    })
-}
+import { sendMessagePost } from "../services/apiPost.js";
+import { sanitizePayload } from "../helpers/sanitizacionDeObjetos.js";
+import { messageSendAlert } from "../utils/sweetsAlerts.js";
+export const sendMessage = (formContact) => {
+  formContact.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(formContact);
+    const payload = Object.fromEntries(formData);
+    // Sanitizamos el objeto de caracteres maliciosos
+    const data = sanitizePayload(payload);
+    if (!data) return alert("No debe haber datos vacios en el formulario.");
+
+    //Enviamos el mensaje al backend
+    const resp = await sendMessagePost(data);
+    if (resp) {
+      messageSendAlert();
+    }
+
+    // Limpiamos los input de los formularios
+    const cajas = formContact.querySelectorAll("input, textarea");
+    cajas.forEach((element) => {
+      element.value = "";
+    });
+  });
+};
--- src/js/utils/sweetsAlerts.js
+++ src/js/utils/sweetsAlerts.js
@@ -5,13 +5,20 @@ export const messageSendAlert = () => {
     title: "Mensaje enviado",
     icon: "success",
     draggable: true,
-    width: "550px",                 
-  padding: "5rem",                 
-    confirmButtonText: "Aceptar",   
-  customClass: {
-    title: "swal-title-custom",   
-    icon: "swal-icon-custom",     
-      confirmButton: "swal-btn-custom", 
-  },
+    width: "550px",
+    padding: "5rem",
+    confirmButtonText: "Aceptar",
+    customClass: {
+      title: "swal-title-custom",
+      icon: "swal-icon-custom",
+      confirmButton: "swal-btn-custom",
+    },
+  }).then((result) => {
+    const modal = document.querySelector(".modal");
+    const containerModal = document.querySelector(".containe-modal");
+    if (result.isConfirmed) {
+      modal.classList.add("ocultar-modal");
+      containerModal.classList.remove("traslateY");
+    }
   });
 };
