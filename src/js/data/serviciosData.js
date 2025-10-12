c078fd7be36c8d37d6147245f370e4454f323269
LuisLunaV <luis.a.luna.v@outlook.com>
2025-10-11 23:13:54 -0600

    cambiamos los assets

--- src/js/data/serviciosData.js
+++ src/js/data/serviciosData.js
@@ -1,20 +1,20 @@
 export const serviciosCard=[
     {
         id: 'redes',
-        component:`<img src="./src/assets/img/redesCCTV.png" alt="servicio">`
+        component:`<img src="./assets/img/redesCCTV.png" alt="servicio">`
     },
     {
         id: 'internet',
-        component:`<img src="src/assets/img/publicidad/netweb.jpeg" alt="servicio">`
+        component:`<img src="./assets/img/publicidad/netweb.jpeg" alt="servicio">`
     },    {
         id: 'web',
-        component:`<img src="src/assets/img/publicidad/devweb.png" alt="servicio">`
+        component:`<img src="./assets/img/publicidad/devweb.png" alt="servicio">`
     },{
         id: 'sistemas',
-        component:`<img src="src/assets/img/publicidad/sistemas.png" alt="servicio">`
+        component:`<img src="./assets/img/publicidad/sistemas.png" alt="servicio">`
     },
     {
         id: 'consultoria',
-        component:`<img src="src/assets/img/publicidad/asesorias.jpeg" alt="servicio">`
+        component:`<img src="./assets/img/publicidad/asesorias.jpeg" alt="servicio">`
     }
 ]
