41e456d47facb5d50cef90e76bfce88482014d92
LuisLunaV <luis.a.luna.v@outlook.com>
2025-10-25 00:22:53 -0600

    Agregamos imagenes .webp

--- src/js/data/serviciosData.js
+++ src/js/data/serviciosData.js
@@ -1,20 +1,20 @@
 export const serviciosCard=[
     {
         id: 'redes',
-        component:`<img src="./assets/img/redesCCTV.png" alt="servicio">`
+        component:`<img src="./assets/img/redesCCTV.webp" alt="servicio">`
     },
     {
         id: 'internet',
-        component:`<img src="./assets/img/publicidad/netweb.jpeg" alt="servicio">`
+        component:`<img src="./assets/img/publicidad/netweb.webp" alt="servicio">`
     },    {
         id: 'web',
-        component:`<img src="./assets/img/publicidad/devweb.png" alt="servicio">`
+        component:`<img src="./assets/img/publicidad/devweb.webp" alt="servicio">`
     },{
         id: 'sistemas',
-        component:`<img src="./assets/img/publicidad/sistemas.png" alt="servicio">`
+        component:`<img src="./assets/img/publicidad/sistemas.webp" alt="servicio">`
     },
     {
         id: 'consultoria',
-        component:`<img src="./assets/img/publicidad/asesorias.jpeg" alt="servicio">`
+        component:`<img src="./assets/img/publicidad/asesorias.webp" alt="servicio">`
     }
 ]
